47419e70172c3dc35e09b297a19787e9b4881ff8
Admin <ivancovs982@gmail.com>
2025-05-23 10:10:12 +0300

    add connect db

--- src/repository/studentRepository.js
+++ src/repository/studentRepository.js
@@ -1,80 +1,99 @@
 import {Students as Student} from "../model/students.js";
-const students = new Map();
+import {MongoClient} from "mongodb";
+const url = ' mongodb://edd:1234@localhost:27017/java59?authSource=admin';
+const dbName = 'java59';
+const client = new MongoClient(url)
+let collection;
 
-export const addStudent = ({id, name, password}) => {
-    if (students.has(id)) {
+export async function connect() {
+    // if (!(client.topology && client.topology.isConnected())) {
+    //     await client.connect();
+    // }
+    if (!client.topology?.isConnected()) {
+        await client.connect();
+    }
+    await client.connect();
+    const db = client.db(dbName);
+    collection = db.collection('college');
+}
+
+
+export const addStudent = async ({id, name, password}) => {
+    await connect();
+    const existing = await collection.findOne({_id: id});
+    if (existing) {
         return false;
     }
-    students.set(id, new Student(id, name, password));
+    await collection.insertOne({_id: id, name, password: password, scores:{}});
     return true;
 }
-
-export const findStudent = (id) =>{
-    return students.get(id)
-}
-
-export const deleteStudent = (id) =>{
-    const student = students.get(id);
-    if (student){
-        students.delete(id);
-        return students;
-    }
-}
-
-export const updateStudent = (id, data) =>{
-    const student = students.get(id);
-    if (student){
-        Object.assign(student, data);
-        return student;
-    }
-}
-export const addScore = ({ id, examName, score }) => {
-    console.log("Trying to find student with ID:", id);
-
-    const student = students.get(Number(id));
-
-    if (!student) {
-        console.log("Student not found!");
-        return false;
-    }
-
-    student.scores[examName] = score;
-    console.log("Updated scores:", student.scores);
-
-    return true;
-};
-
-export const searchByName = (name) => {
-    const matchingStudents = [];
-
-    students.forEach((student) => {
-        if (student.name.toLowerCase() === name.toLowerCase()) {
-            matchingStudents.push(student);
-        }
-    });
-
-    return matchingStudents;
-};
-export const countByNames = (namesArray) => {
-    let count = 0;
-
-    students.forEach((student) => {
-        if (namesArray.includes(student.name)) {
-            count++;
-        }
-    });
-
-    return count;
-};
-export const findByMinScore = (exam, minScore) => {
-    const matchingStudents = [];
-
-    students.forEach((student) => {
-        if (student.scores[exam] !== undefined && student.scores[exam] >= minScore) {
-            matchingStudents.push(student);
-        }
-    });
-
-    return matchingStudents;
-};
+//
+// export const findStudent = (id) =>{
+//     return students.get(id)
+// }
+//
+// export const deleteStudent = (id) =>{
+//     const student = students.get(id);
+//     if (student){
+//         students.delete(id);
+//         return students;
+//     }
+// }
+//
+// export const updateStudent = (id, data) =>{
+//     const student = students.get(id);
+//     if (student){
+//         Object.assign(student, data);
+//         return student;
+//     }
+// }
+// export const addScore = ({ id, examName, score }) => {
+//     console.log("Trying to find student with ID:", id);
+//
+//     const student = students.get(Number(id));
+//
+//     if (!student) {
+//         console.log("Student not found!");
+//         return false;
+//     }
+//
+//     student.scores[examName] = score;
+//     console.log("Updated scores:", student.scores);
+//
+//     return true;
+// };
+//
+// export const searchByName = (name) => {
+//     const matchingStudents = [];
+//
+//     students.forEach((student) => {
+//         if (student.name.toLowerCase() === name.toLowerCase()) {
+//             matchingStudents.push(student);
+//         }
+//     });
+//
+//     return matchingStudents;
+// };
+// export const countByNames = (namesArray) => {
+//     let count = 0;
+//
+//     students.forEach((student) => {
+//         if (namesArray.includes(student.name)) {
+//             count++;
+//         }
+//     });
+//
+//     return count;
+// };
+// export const findByMinScore = (exam, minScore) => {
+//     const matchingStudents = [];
+//
+//     students.forEach((student) => {
+//         if (student.scores[exam] !== undefined && student.scores[exam] >= minScore) {
+//             matchingStudents.push(student);
+//         }
+//     });
+//
+//     return matchingStudents;
+// };
 
--- src/routs/studentRouter.js
+++ src/routs/studentRouter.js
@@ -1,23 +1,18 @@
 import express from "express";
 import {
-    addScore,
-    addStudent, deleteStudent,
-    findByMinScore,
-    findByName,
-    findStudent,
-    updateStudent
+    addStudent
 } from "../controler/studentControler.js";
 
 
 export const router = express.Router();
 
 router.post('/student', addStudent)
-router.get('/student/:id', findStudent)
-router.delete('/student/:id', deleteStudent)
-router.patch('/student/:id', updateStudent)
-router.patch('/score/student/:id', addScore)
-router.get('/students/name/:name', findByName)
-router.get('/quantity/students/', findStudent)
-router.get('/students/exam/:exam/minScore/:minScore', findByMinScore)
+// router.get('/student/:id', findStudent)
+// router.delete('/student/:id', deleteStudent)
+// router.patch('/student/:id', updateStudent)
+// router.patch('/score/student/:id', addScore)
+// router.get('/students/name/:name', findByName)
+// router.get('/quantity/students/', findStudent)
+// router.get('/students/exam/:exam/minScore/:minScore', findByMinScore)
 
 export default router;
